077e4d67eb3b70cceee2cb66081ce725bed166e1
Julian <julian.salamanca@est.iudigital.edu.co>
2025-05-02 00:10:38 -0500

    Creacion lista de restaurantes

---
+++ restaurantes.js
@@ -0,0 +1,46 @@
+const restaurantes =[
+    {
+        name:"La Pizzeria Italiana",
+        estrellas:"⭐ 5",
+        direccion:"Cr 45 70-82",
+        imagen:"restaurante1.png",
+        categoria:"Italiano"
+    },
+    {
+        name:"El Sabor Mexicano",
+        estrellas:"⭐ 4",
+        direccion:"Cl 60 10-20",
+        imagen:"restaurante2.png",
+        categoria:"Mexicano"
+    },
+    {
+        name:"Sushi House",
+        estrellas:"⭐ 5",
+        direccion:"Dg 1 22-30",
+        imagen:"restaurante3.png",
+        categoria:"Mar"
+    },
+    {
+        name:"Italiano 65",
+        estrellas:"⭐ 4.5",
+        direccion:"Av 45 1-85",
+        imagen:"imagen3.png",
+        categoria:"Italiano"
+    },
+    {
+        name:"Fritos y Mas",
+        estrellas:"⭐ 3.5",
+        direccion:"Cl 45 30b-71",
+        imagen:"imagen2.png",
+        categoria:"Mexicano"
+    },
+    {
+        name:"Pacifico mar",
+        estrellas:"⭐ 4",
+        direccion:"Tv 82 Sur 67-90",
+        imagen:"imagen1.png",
+        categoria:"Mar"
+    }
+];
+
+export {restaurantes};
