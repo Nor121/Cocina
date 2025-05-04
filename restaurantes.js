2753e4a025764464030173698ed41f4ac87956f9
Julian <julian.salamanca@est.iudigital.edu.co>
2025-05-04 17:06:53 -0500

    Mejoras en diseño

--- restaurantes.js
+++ restaurantes.js
@@ -2,70 +2,70 @@ const restaurantes =[
     {
         name:"La Pizzeria Italiana",
         estrellas:"⭐ 5",
-        direccion:"Cr 45 70-82",
+        direccion:"Av. 10 # 15 - 80 Bogotá",
         imagen:"restaurantes/restaurante1.png",
         categoria:"Italiano"
     },
     {
         name:"El Sabor Mexicano",
         estrellas:"⭐ 4",
-        direccion:"Cl 60 10-20",
+        direccion:"Cra 12 # 50 - 15 Barranquilla",
         imagen:"restaurantes/restaurante2.png",
         categoria:"Mexicano"
     },
     {
         name:"Sushi House",
         estrellas:"⭐ 5",
-        direccion:"Dg 1 22-30",
+        direccion:"Cra 20 # 45 - 90 Cali",
         imagen:"restaurantes/restaurante3.png",
         categoria:"Mar"
     },
     {
         name:"Italiano 65",
         estrellas:"⭐ 4.5",
-        direccion:"Av 45 1-85",
+        direccion:"Av. 45 # 1 - 85 Bogotá",
         imagen:"restaurantes/imagen3.png",
         categoria:"Italiano"
     },
     {
         name:"Fritos y Mas",
         estrellas:"⭐ 3.5",
-        direccion:"Cl 45 30b-71",
+        direccion:"Cl 45 # 30B - 71 Medellín",
         imagen:"restaurantes/imagen2.png",
         categoria:"Mexicano"
     },
     {
         name:"Pacifico mar",
         estrellas:"⭐ 4",
-        direccion:"Tv 82 Sur 67-90",
+        direccion:"Tv. 82 SUR # 67A - 90 Cali",
         imagen:"restaurantes/imagen1.png",
         categoria:"Mar"
     },
     {
         name:"Rocoto",
         estrellas:"⭐ 4.7",
-        direccion:"Cr 35 50-67",
+        direccion:"Cra 35C # 50 - 67 Barranquilla",
         imagen:"restaurantes/rocoto.avif",
         categoria:"Mar"
     },
     {
         name:"Asado y Parrillas",
         estrellas:"⭐ 3.8",
-        direccion:"Cl 10 Sur 65-20",
+        direccion:"Cl 10 SUR # 65 - 20 Medellín",
         imagen:"restaurantes/AsadoParrilla.jpg",
         categoria:"Italiano"
     },
     {
         name:"Delicias Marinas",
         estrellas:"⭐ 4",
-        direccion:"Cr 15 21-47 Norte",
+        direccion:"Cra 15 # 21 - 47 Norte Bogotá",
         imagen:"restaurantes/DeliciasMarinas.jpg",
         categoria:"Mar"
     },
     {
         name:"Gourmet Express",
         estrellas:"⭐ 5",
-        direccion:"Dg 45 29-243",
+        direccion:"Dg 45 # 29F - 243 Cali",
         imagen:"restaurantes/GourmetExpress.jpg",
         categoria:"Mexicano"
     }
